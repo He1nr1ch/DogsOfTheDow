67c260dc0374b44a5740da6515fab0b195aa3aba
iheinrich@gmail.com <iheinrich@gmail.com>
2021-10-19 11:12:52 -0400

    added title

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python
 
+#the Dow stocks are DIS, BA, V, AAPL, NKE, MSFT, AXP, UNH, WMT, HD, GS, MCD, CAT, TRV, JPM, PG, INTC, JNJ, CSCO, KO, MMM, MRK, PFE, WBA, IBM, DOW, CVX, VZ, XOM
+
+import streamlit as st
+
+st..title('Dogs of the Dow Calculator')
